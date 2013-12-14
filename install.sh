ddd7d973135abf8de8ca46685a47b23e8baf1606
malcolm m <malcolm@hoprocker.net>
2013-12-14 13:48:55 -0800

    Add a message for when install finishes

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ apt-get install -y firmware-atheros hostapd wireless-tools bridge-tools
 curl -o - "$REPO/interfaces" > /etc/network/interfaces
 curl -o - "$REPO/hostapd.conf" > /etc/hostapd/hostapd.conf
 echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
+
+echo ""
+echo "This system has been configured. Now run 'shutdown -r now' as root to complete install."
