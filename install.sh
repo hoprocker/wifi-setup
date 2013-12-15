9b5da60c1f7990210736fe2f62a6f91551a65c28
malcolm m <malcolm@hoprocker.net>
2013-12-15 13:07:11 -0800

    correct package name

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 REPO="https://raw.github.com/hoprocker/wifi-setup/master"
 curl -o - "$REPO/sources.list" >> /etc/apt/sources.list
 apt-get update
-apt-get install -y firmware-atheros hostapd wireless-tools bridge-tools
+apt-get install -y firmware-atheros hostapd wireless-tools bridge-utils
 curl -o - "$REPO/interfaces" > /etc/network/interfaces
 curl -o - "$REPO/hostapd.conf" > /etc/hostapd/hostapd.conf
 echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
